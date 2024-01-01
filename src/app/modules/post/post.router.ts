b87484f9cc208dd2583e8607ca2576812c23b347
tonmoy-kumar-roy <tonmoy.roy@selisegroup.com>
2024-01-01 09:53:56 +0600

    add authorization in post route

--- src/app/modules/post/post.router.ts
+++ src/app/modules/post/post.router.ts
@@ -1,24 +1,18 @@
 import express from "express";
-import { createPostByUser,getIndividualPost,getOwnPosts,getPosts, likePost } from './post.controller';
+import { createPostByUser, getIndividualPost, getOwnPosts, getPosts, likePost } from './post.controller';
+import { authentication } from "../../../middleware/authentication.middleware";
 const router = express.Router();
 
-router.post('/new/post',createPostByUser)
-router.get('/posts/:loggedInUserId',getPosts)
+router.post('/new/post', authentication, createPostByUser);
+router.get('/posts/:loggedInUserId', authentication, getPosts);
 
 //users own post will show
-router.get('/own/posts/:userName',getOwnPosts)
+router.get('/own/posts/:userName', getOwnPosts);
 
 //individual post
-router.get('/post/:postId',getIndividualPost)
+router.get('/post/:postId', getIndividualPost);
 
 //like or unlike a post by user
-router.post('/like/:postId',likePost)
-
-
-// router.post('/register-google-user', googleAuthentication, registerByGoogle);
-// router.post('/registration-request', registerRequest)
-// router.post('/registration-confirm', registerUser)
-// router.get('/data', authentication, getUserByToken)
-// router.patch('/update', authentication, updateUserByToken);
+router.post('/like/:postId', likePost);
 
 export default router;
